5ddee7ead814419a4891610ea4de7195217210cc
itisameerkhan <itisameerkhan@gmail.com>
2023-12-15 09:29:34 +0530

    15/12/2023  09:29

--- src/Confirm.jsx
+++ src/Confirm.jsx
@@ -3,7 +3,7 @@ import './Confirm.css';
 const Confirm = () => {
     return (
         <div className="confirm">
-            <img src="/src/assets/images/icon-thank-you.svg" alt="" />
+            <img src="./assets/images/icon-thank-you.svg" alt="" />
             <h1>Thank you!</h1>
             <p>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.</p>
         </div>
